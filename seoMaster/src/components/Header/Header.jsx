54c66fe93f1938cb546ddd8357edb61d563c5312
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-31 14:45:54 -0300

    feat: adição de header

--- seoMaster/src/components/Header/Header.jsx
+++ seoMaster/src/components/Header/Header.jsx
@@ -1,30 +1,38 @@
 import { Conteiner } from "./styles";
+import { ButtonMenu } from "../Button/styles";
+import { FaSearch } from 'react-icons/fa';
 function Header() {
   return (
     <Conteiner>
-      <a href="">
-        <img src="" alt="Logo do site" className="logo" />
-      </a>
-
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Service</a>
-          </li>
-          <li>
-            <a href="#">Project</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="align">
+          <a href="">
+            <img src="" alt="Logo do site" className="logo" />
+          </a>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Service</a>
+              </li>
+              <li>
+                <a href="#">Project</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+                  </ul>
+                  
+                  <div className="buttons">
+                      <FaSearch className="icon"/>
+                      <ButtonMenu>Pro Version</ButtonMenu>
+                  </div>
+          </nav>
+      </div>
     </Conteiner>
   );
 }
--- seoMaster/src/components/Header/styles.js
+++ seoMaster/src/components/Header/styles.js
@@ -1,17 +1,47 @@
 import { styled } from "styled-components";
 import { colors } from "../../helpers/colors";
 
-import { displayFlex } from "../styles";
+import { align, displayFlex } from "../styles";
 
 
 const Conteiner = styled.header`
-  background: blue;
-  color: ${colors.white};
-  ul{
+  background: ${colors.blue[200]};
+  border-bottom: 1px solid ${colors.blue[300]};
+  height: 100px;
+  .align,
+  ul,
+  .buttons,
+  nav {
     ${displayFlex};
+    height: inherit;
   }
-  li a:hover{
-    color: ${colors.blue};
+
+  .align {
+    ${align}
+  }
+
+  nav{
+    align-items: baseline;
+  }
+  * {
+    color: ${colors.white};
+  }
+
+  ul {
+    width: 425px;
+  }
+  li a:hover {
+    color: ${colors.blue[400]};
+  }
+
+  .buttons {
+    width: fit-content;
+    .icon path {
+      color: ${colors.blue[400]};
+    }
+    button {
+      margin-left: 25px;
+    }
   }
 `;
 
