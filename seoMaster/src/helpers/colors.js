5b3ae90314607330f1c5b818404b2fca66d92a1c
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-31 15:48:19 -0300

    feat: adição de cores

--- seoMaster/src/helpers/colors.js
+++ seoMaster/src/helpers/colors.js
@@ -1,18 +1,15 @@
 const colors = {
-    white: "#fff",
-    whiteLigth: "#f7faff",
-    blue: {
-        "200": "#2124b1",
-        "400": "#4777f5",
-        "300": "#383ab9",
-        "500":"#5985f6",
-        "600":"#638bf7"
-    },
-    black: "#1d1d27",
-    grey: "#888"
-}
+  white: "#fff",
+  whiteLigth: "#f7faff",
+  blue: {
+    200: "#2124b1",
+    300: "#383ab9",
+    400: "#4777f5",
+    500: "#5985f6",
+    600: "#638bf7",
+  },
+  black: "#1d1d27",
+  grey: "#888",
+};
 
-export {
-    colors
-}
-
+export { colors };
