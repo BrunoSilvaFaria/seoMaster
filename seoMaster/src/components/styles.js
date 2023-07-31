9938257fed03e30d99fa0c101a30d512e9f9a193
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-31 14:46:07 -0300

    feat: adição de estilos

--- seoMaster/src/components/styles.js
+++ seoMaster/src/components/styles.js
@@ -1,6 +1,13 @@
 import { css } from "styled-components";
 const displayFlex = css`
   display: flex;
+  align-items: center;
+  justify-content: space-around;
 `;
 
-export { displayFlex };
+const align = css`
+  width: 90%;
+  margin: auto;
+`;
+
+export { displayFlex, align };
