b55324eb0af6d11dd62ccd622d14739bb852bdf3
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-01 11:00:12 -0300

    feat: adição de serviços oferecidos

---
+++ seoMaster/src/data/services.js
@@ -0,0 +1,40 @@
+const services = [
+  {
+    title: "SEO Optimization",
+    description:
+      "Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.",
+    id: 1,
+  },
+  {
+    title: "Web Design",
+    description:
+      "Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.",
+    id: 2,
+  },
+  {
+    title: "Social Media Marketing",
+    description:
+      "Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.",
+    id: 3,
+  },
+  {
+    title: "Email Marketing",
+    description:
+      "Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.",
+    id: 4,
+  },
+  {
+    title: "PPC Advertising",
+    description:
+      "Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.",
+    id: 5,
+  },
+  {
+    title: "App Development",
+    description:
+      "Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.",
+    id: 6,
+  },
+];
+
+export default services;
