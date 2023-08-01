28149a52b2b1971580b9d8ce376e369d9edf1f34
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-01 10:01:49 -0300

    feat: adição de rotas, para as paginas

--- seoMaster/src/App.jsx
+++ seoMaster/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router";
 function App() {
   return (
     <>
       <Header />
+      <Outlet/>
       <Footer />
     </>
   );
--- seoMaster/src/main.jsx
+++ seoMaster/src/main.jsx
@@ -1,10 +1,23 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./global.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+
+import Services from "./pages/Services/Services.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/services",
+        element: <Services />,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
