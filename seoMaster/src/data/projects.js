635e62a7e2d1156633872d1549d23d4b43ef6635
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-01 14:50:20 -0300

    feat: adição de categoria

---
+++ seoMaster/src/data/projects.js
@@ -0,0 +1,40 @@
+const services = [
+  {
+    title: "Project Name",
+    type: "Web Design",
+    types: ["design"],
+    id: 1,
+  },
+  {
+    title: "Project Name",
+    type: "Web Design",
+    types: ["development"],
+    id: 2,
+  },
+  {
+    title: "Project Name",
+    type: "Web Design",
+    types: ["design"],
+    id: 3,
+  },
+  {
+    title: "Project Name",
+    type: "Web Design",
+    types: ["development"],
+    id: 4,
+  },
+  {
+    title: "Project Name",
+    type: "Web Design",
+    types: ["design"],
+    id: 5,
+  },
+  {
+    title: "Project Name",
+    type: "Web Design",
+    types: ["development"],
+    id: 6,
+  },
+];
+
+export default services;
