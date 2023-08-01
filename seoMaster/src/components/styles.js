8b10652667b04f4e362d40c1d237dda077cce1b4
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-01 15:50:22 -0300

    feat: adição de estilos que se repetem

--- seoMaster/src/components/styles.js
+++ seoMaster/src/components/styles.js
@@ -10,4 +10,28 @@ const align = css`
   margin: auto;
 `;
 
-export { displayFlex, align };
+const card = css`
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  border-radius: 10px;
+`;
+
+const cardSmall = css`
+  ${card};
+  @media (max-width: 990px) {
+    width: 336px;
+  }
+  @media (min-width: 576px) and (max-width: 765px) {
+    width: 516px;
+  }
+  @media (max-width: 575px) {
+    width: 100%;
+  }
+`;
+const cards = css`
+  ${displayFlex};
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 30px;
+`;
+
+export { displayFlex, align, cards, card, cardSmall };
