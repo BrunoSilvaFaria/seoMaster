997fe39e3a94b233c2943f4597501963ce82fcb8
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-01 15:50:53 -0300

    feat: adição de página que contém informações da empresa, no momento, foi adicionado informações sobre os membros

---
+++ seoMaster/src/pages/About/About.jsx
@@ -0,0 +1,11 @@
+import { Conteiner } from "./styles";
+import Members from "../../components/Card/Member/Members";
+function About() {
+  return (
+    <Conteiner>
+      <Members />
+    </Conteiner>
+  );
+}
+
+export default About;
---
+++ seoMaster/src/pages/About/styles.js
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+import { align } from "../../components/styles";
+
+export const Conteiner = styled.section`
+  ${align};
+  margin: 100px auto;
+`;
