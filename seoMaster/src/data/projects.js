fa64fb91aa3f1e5619b19740cd26088ab38001e0
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-08-03 15:32:27 -0300

    feat: adição de imagens

--- seoMaster/src/data/projects.js
+++ seoMaster/src/data/projects.js
@@ -4,36 +4,42 @@ const services = [
     type: "Web Design",
     types: ["design"],
     id: 1,
+    img: './assets/portfolio-1.jpg',
   },
   {
     title: "Project Name",
     type: "Web Design",
     types: ["development"],
     id: 2,
+    img: './assets/portfolio-2.jpg',
   },
   {
     title: "Project Name",
     type: "Web Design",
     types: ["design"],
     id: 3,
+    img: './assets/portfolio-3.jpg',
   },
   {
     title: "Project Name",
     type: "Web Design",
     types: ["development"],
     id: 4,
+    img: './assets/portfolio-4.jpg',
   },
   {
     title: "Project Name",
     type: "Web Design",
     types: ["design"],
     id: 5,
+    img: './assets/portfolio-5.jpg',
   },
   {
     title: "Project Name",
     type: "Web Design",
     types: ["development"],
     id: 6,
+    img: './assets/portfolio-6.jpg',
   },
 ];
 
